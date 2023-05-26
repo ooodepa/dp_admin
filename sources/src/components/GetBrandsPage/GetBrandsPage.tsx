346754a915489a6698a0c3f89f5ee839c96fb32c
PavelHalanin <PavelHalanin@gmail.com>
2023-05-26 16:18:47 +0300

    main : json/xlsx save added for brands page

--- sources/src/components/GetBrandsPage/GetBrandsPage.tsx
+++ sources/src/components/GetBrandsPage/GetBrandsPage.tsx
@@ -1,3 +1,4 @@
+import * as xlsx from 'xlsx';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -6,6 +7,7 @@ import TableView from '../TableView/TableView';
 import FetchUsers from '../../utils/FetchBackend/rest/api/users';
 import FetchItemBrand from '../../utils/FetchBackend/rest/api/item-brands';
 import { AsyncAlertExceptionHelper } from '../../utils/AlertExceptionHelper';
+import BrowserDownloadFileController from '../../package/BrowserDownloadFileController';
 
 export default function GetBrandsPage() {
   const navigate = useNavigate();
@@ -64,11 +66,70 @@ export default function GetBrandsPage() {
     }
   }
 
+  function saveAsJson() {
+    const filename = 'DP_CTL_ItemBrands.json';
+    const text = JSON.stringify(brands, null, 2);
+    BrowserDownloadFileController.downloadFile(filename, text);
+  }
+
+  async function saveItemsAsXlsx() {
+    try {
+      const headers = [
+        'id',
+        'Сортировка',
+        'Наименование',
+        'Картинка',
+        'Ссылка',
+        'Ключевые слова',
+        'Описание',
+        'Скрыт',
+      ];
+
+      const workbook = xlsx.utils.book_new();
+
+      const arr = brands.sort((a, b) => a.dp_sortingIndex - b.dp_sortingIndex);
+
+      const data: string[][] = [];
+
+      for (let j = 0; j < arr.length; ++j) {
+        const brand = arr[j];
+        data.push([
+          `${brand.dp_id}`,
+          `${brand.dp_sortingIndex}`,
+          brand.dp_name,
+          brand.dp_photoUrl,
+          brand.dp_urlSegment,
+          brand.dp_seoKeywords,
+          brand.dp_seoDescription,
+          brand.dp_isHidden ? '1' : '0',
+        ]);
+      }
+
+      const worksheet = xlsx.utils.aoa_to_sheet([headers, ...data]);
+      xlsx.utils.book_append_sheet(workbook, worksheet, `all`);
+
+      const excelBuffer = xlsx.write(workbook, {
+        bookType: 'xlsx',
+        type: 'array',
+      });
+
+      const filename = 'DP_CTL_ItemBrands.xlsx';
+      const blob = new Blob([excelBuffer], {
+        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      });
+      BrowserDownloadFileController.downloadFileByBlob(filename, blob);
+    } catch (exception) {
+      await AsyncAlertExceptionHelper(exception, navigate);
+    }
+  }
+
   return (
     <TableView
       side={
         <>
           <button onClick={() => navigate('new/create')}>Создать новый</button>
+          <button onClick={saveAsJson}>Скачать как JSON</button>
+          <button onClick={saveItemsAsXlsx}>Скачать как XLSX</button>
         </>
       }>
       {modal}
