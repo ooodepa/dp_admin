549c58fbef44533a926dd872ae3faddb1327a755
pavelhalanin <pavel.innokentevich.galanin@gmail.com>
2024-03-22 14:19:56 +0300

    main: redux added

--- .bash_history
+++ .bash_history
@@ -5,3 +5,8 @@ mv dp-admin/.* ./
 yarn add react-router-dom"
 yarn add -D prettier
 yarn add xlsx
+yarn add redux
+yarn add react-redux
+yarn add @reduxjs/toolkit
+yarn add redux-thunk
+yarn add -D @types/react-redux
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import { HashRouter, BrowserRouter } from 'react-router-dom';
 
 import './index.css';
+import { store } from './store';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
@@ -10,13 +12,17 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   process.env.REACT_APP__USE_BROWSER_ROUTER === 'true' ? (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   ) : (
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
   ),
 );
 
